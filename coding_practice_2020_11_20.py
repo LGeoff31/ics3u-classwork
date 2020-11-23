e5b88a42af0d98d0f87174729edc2e55abda538b
LGeoff31 <74619195+LGeoff31@users.noreply.github.com>
2020-11-23 12:21:56 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -1,1 +1,11 @@
-https://repl.it/@GeoffreyLee/4-questions#main.py
+color = input('What is your favorite color? :')
+print(f"{color}?!, No way, that's my favourite color too!")
+
+number_of_cans = int(input('Please enter amount of cans in a pack :'))
+number_of_packs = int(input('Please enter amount of packs :'))
+print(number_of_cans * number_of_packs, 'cans in total!')
+
+h = int(input('Please enter the height of prism :'))
+w = int(input('Please enter the width of prism :'))
+l = int(input('Please enter the height of prism :'))
+print( h * w * l, 'units cubed!')
