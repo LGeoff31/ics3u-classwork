67903839487791573e79203996108a863e0eaf1d
LGeoff31 <74619195+LGeoff31@users.noreply.github.com>
2020-11-27 13:59:41 -0500

    Create coding_practice_2020_11_27.py

---
+++ coding_practice_2020_11_27.py
@@ -0,0 +1,69 @@
+#from 83-377 by 7
+
+i = 83
+while i <= 377:
+    print(i)
+    i += 7
+
+#from -252 to 594 by 18
+
+i = -252
+while i <= 594:
+    print(i)
+    i += 18
+
+#from 250 to 397 by 3
+
+i = 250
+while i <=397:
+    print(i)
+    i+=3
+
+#from 46 to 116 by 7
+
+i = 46
+while i <=116:
+    print(i)
+    i+=7
+
+#from 170 to 258 by 8
+
+i = 170
+while i <=258:
+    print(i)
+    i+=8
+
+#from 367 to 945 by 17
+
+i = 367
+while i <= 945:
+    print(i)
+    i+=17
+
+#from -80 to 243 by 17
+
+i = -80
+while i <=243:
+    print(i)
+    i+=17
+
+#from -267 to -195 by 3
+
+i = -267
+while i <=-195:
+    print(i)
+    i+=3
+
+#from -410 to -215 by 5
+
+i = -410
+while i <= -215:
+    print(i)
+    i+=5
+
+#from 226 to 280 by 3
+
+i = 226
+while i <= 280:
+    print(i)
+    i+=3
